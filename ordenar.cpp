182a9913c8cc2a1331e05118c53d6d9679b63fcb
BossioCervantes <124269531+BossioCervantes@users.noreply.github.com>
2023-02-02 08:54:11 -0500

    Create ordenar.cpp

---
+++ ordenar.cpp
@@ -0,0 +1,14 @@
+lista = []
+
+orden1 = int(input("ingresar el primer digito"))
+orden2 = int(input("ingresar el segundo digito"))
+orden3 = int(input("ingresar el tercer digito"))
+orden4 = int(input("ingresar el cuarto digito"))
+
+lista.append(orden1)
+lista.append(orden2)
+lista.append(orden3)
+lista.append(orden4)
+
+lista.sort()
+print(lista) 
